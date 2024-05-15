1f2ab00e2c266786126cf058f1cfabd9fa04978c
AbdullahTrees <43266058+AbdullahTrees@users.noreply.github.com>
2024-05-16 03:18:27 +0600

    Create Cmake project

---
+++ StopRightNow/StopRightNow.cpp
@@ -0,0 +1,11 @@
+﻿// StopRightNow.cpp : Defines the entry point for the application.
+//
+
+#include "StopRightNow.h"
+
+using namespace std;
+
+void SRN_init(void)
+{
+
+}
---
+++ StopRightNow/main.cpp
@@ -0,0 +1,15 @@
+// main.cpp : Defines the entry point for the application.
+//
+
+#include "StopRightNow.h"
+
+using namespace std;
+
+int main()
+{
+	SRN_init();
+	
+
+
+	return 0;
+}
