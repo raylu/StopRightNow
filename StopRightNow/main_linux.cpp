0dd43fece327bc1c98c2e94ab44b44660ddb18af
raylu <90059+raylu@users.noreply.github.com>
2025-04-06 21:58:48 -0700

    bazel

--- StopRightNow/main_linux.cpp
+++ StopRightNow/main_linux.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
+
 int main_linux(void)
 {
-	cout << "Hoi! If you're seeing this, you're on Linux! As a test, I shall proceed to use some Linux only functions!\n";
+	std::cout << "Hoi! If you're seeing this, you're on Linux! As a test, I shall proceed to use some Linux only functions!\n";
 
 	return 0;
 }
